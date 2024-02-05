881d54aa4512d27cbd340e1b5d0096428e85ee79
Yashraj Narke <ynnarke52@gmail.com>
2024-02-05 16:03:41 +0530

    d

--- api/resize.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-export default async function (
-  request : VercelRequest,
-  response : VercelResponse
-) {
-  response.send("daa");
-}
